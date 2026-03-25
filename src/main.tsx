47722a602ce6c3f9b5306a0a2a61aa0da0f15114
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2026-03-25 23:53:38 +0530

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+
+root.render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
